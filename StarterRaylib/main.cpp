a45ce787ff6d2f9f15c75703e9b05870c8456142
squallcoder <tammapavan38@gmail.com>
2025-02-17 19:40:56 -0500

    Background Initial Commit

--- StarterRaylib/main.cpp
+++ StarterRaylib/main.cpp
@@ -12,6 +12,17 @@ typedef struct Player {
     bool isJumping;
 } Player;
 
+typedef struct 
+{
+    Vector2 position;
+    float radius;
+    float alpha;
+    float speed;
+    Color color;
+    
+} Particle;
+
+
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Platformer");
     SetTargetFPS(60);
@@ -22,20 +33,53 @@ int main(void) {
     // Floor
     Rectangle floor = {0, SCREEN_HEIGHT - 100, SCREEN_WIDTH, 100};
 
+    // Test Particle
+    Color colors[14] = { ORANGE, RED, GOLD, LIME, BLUE, VIOLET, BROWN, LIGHTGRAY, PINK,
+        YELLOW, GREEN, SKYBLUE, PURPLE, BEIGE };
+
+    Particle circles[10] = { 0 };
+
+    for (int i = 10 - 1; i >= 0; i--)
+    {
+        circles[i].alpha = 0.0f;
+        circles[i].radius = 10;
+        circles[i].position.x = (float)GetRandomValue((int)circles[i].radius, (int)(SCREEN_WIDTH - circles[i].radius));
+        circles[i].position.y = (float)GetRandomValue((int)circles[i].radius, (int)((SCREEN_HEIGHT) - circles[i].radius));
+        circles[i].speed = (float)GetRandomValue(1, 100)/2000.0f;
+        circles[i].color = colors[GetRandomValue(0, 13)];
+    }
+
     while (!WindowShouldClose()) {
         // Move left and right
         if (IsKeyDown(KEY_A)) player.rect.x -= SPEED;
         if (IsKeyDown(KEY_D)) player.rect.x += SPEED;
 
         // Jump
-        if (IsKeyPressed(KEY_W) && !player.isJumping) {
+        if (IsKeyPressed(KEY_SPACE) && !player.isJumping) {
             player.velocity.y = JUMP_STRENGTH;
             player.isJumping = true;
+        }
+        // Spawning Particles
+        if (IsKeyPressed(KEY_P)) {
+            for (int i = 10 - 1; i >= 0; i--)
+            {
+                circles[i].alpha = 0.0f;
+                circles[i].radius = 10;
+                circles[i].position.x = (float)GetRandomValue((int)circles[i].radius, (int)(SCREEN_WIDTH - circles[i].radius));
+                circles[i].position.y = (float)GetRandomValue((int)circles[i].radius, (int)((SCREEN_HEIGHT) - circles[i].radius));
+                circles[i].speed = (float)GetRandomValue(1, 100)/2000.0f;
+                circles[i].color = colors[GetRandomValue(0, 13)];
+            }
         }
 
         // Gravity
         player.velocity.y += GRAVITY;
         player.rect.y += player.velocity.y;
+        for (int i = 10 - 1; i >= 0; i--)
+        {
+            circles[i].position.y += GRAVITY;
+
+        }
 
         // Floor collision
         if (player.rect.y + player.rect.height >= floor.y) {
@@ -43,9 +87,39 @@ int main(void) {
             player.velocity.y = 0;
             player.isJumping = false;
         }
+        for (int i = 10 - 1; i >= 0; i--){
+
+        if (circles[i].position.y + (circles[i].radius) >= floor.y) {
+            circles[i].position.y = floor.y - circles->radius;
+        }
+    }
+
+
+        // for (int i = 10 - 1; (i >= 0); i--)
+        // {
+        //     circles[i].alpha += circles[i].speed;
+        //     circles[i].radius += circles[i].speed*10.0f;
+
+        //     if (circles[i].alpha > 1.0f) circles[i].speed *= -1;
+
+        //     if (circles[i].alpha <= 0.0f)
+        //     {
+        //         circles[i].alpha = 0.0f;
+        //         circles[i].radius = 20;
+        //         circles[i].position.x = (float)GetRandomValue((int)circles[i].radius, (int)(SCREEN_WIDTH - circles[i].radius));
+        //         circles[i].position.y = (float)GetRandomValue((int)circles[i].radius, (int)((SCREEN_HEIGHT - 100) - circles[i].radius));
+        //         circles[i].color = colors[GetRandomValue(0, 13)];
+        //         circles[i].speed = (float)GetRandomValue(1, 100)/2000.0f;
+        //     }
+        // }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        for (int i = 10 - 1; i >= 0; i--)
+        {
+            DrawCircleV(circles[i].position, circles[i].radius, (circles[i].color));
+        }
+
         DrawRectangleRec(floor, DARKGRAY);
         DrawRectangleRec(player.rect, BLUE);
         EndDrawing();
