9be8379a6aa206831d3b07819b7da90eba5d20c6
cbarnes3 <cbarnes1827@gmail.com>
2025-02-20 21:05:15 -0500

    Add logging feature for FPS and frametime

--- StarterRaylib/main.cpp
+++ StarterRaylib/main.cpp
@@ -1,10 +1,16 @@
 #include <raylib.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define GRAVITY 0.5f
 #define JUMP_STRENGTH -10.0f
 #define SPEED 5.0f
+
+using namespace std;
 
 typedef struct Player {
     Rectangle rect;
@@ -22,10 +28,32 @@ int main(void) {
     // Floor
     Rectangle floor = {0, SCREEN_HEIGHT - 100, SCREEN_WIDTH, 100};
 
+    // Stores frametime and FPS
+    vector<pair<float,int>> frameData;
+
+    double startTime = GetTime(); // initialize time
+    double duration = 10.0; // duration to record FPS and frametime
+
     while (!WindowShouldClose()) {
+        double currentTime = GetTime();
+
+        // exits game loop when time duration for recording is passed
+        if (currentTime - startTime >= duration) {
+            break;
+        }
+
         // Move left and right
         if (IsKeyDown(KEY_A)) player.rect.x -= SPEED;
         if (IsKeyDown(KEY_D)) player.rect.x += SPEED;
+
+        float frameTime = GetFrameTime();
+        int fps = GetFPS();
+
+        // Doesnt log data if current time is under 1 second to allow FPS to stablize
+        // else, time is logged
+         if (currentTime - startTime > 1.0) {
+            frameData.push_back({frameTime, fps});
+        }
 
         // Jump
         if (IsKeyPressed(KEY_W) && !player.isJumping) {
@@ -49,6 +77,21 @@ int main(void) {
         DrawRectangleRec(floor, DARKGRAY);
         DrawRectangleRec(player.rect, BLUE);
         EndDrawing();
+    }
+
+
+    // open CSV file and save data to it
+    ofstream file("fps_data.csv");
+    if (file.is_open()){
+        file << "FrameTime,FPS\n";
+        for (const auto& entry : frameData) {
+            file << entry.first << "," << entry.second << "\n";
+        }
+        file.close();
+        cout << "FPS data saved to file\n";
+    }
+    else {
+        cerr << "Failed to open file\n";
     }
 
     CloseWindow();
